a902ca855f54a6e2205979debd9051d77ae923dc
Chris <ckeenan@starry.com>
2019-03-28 15:48:36 -0400

    created ContextConversionFactorProvider class

---
+++ src/androidMain/kotlin/com.chrynan.inlinepixel/ContextConversionFactorProvider.kt
@@ -0,0 +1,33 @@
+package com.chrynan.inlinepixel
+
+import android.content.Context
+import android.view.View
+
+class ContextConversionFactorProvider(context: Context) : ConversionFactorProvider {
+
+    override val dipToPxFactor = context.resources.displayMetrics.density
+
+    override val spToPxFactor = context.resources.displayMetrics.scaledDensity
+
+    override val ptToPxFactor =
+        context.resources.displayMetrics.densityDpi / ConversionFactorProvider.POINT_PIXELS_PER_INCH
+}
+
+class ContextScreenDimensionUnitConverter(context: Context) :
+    BaseScreenDimensionUnitConverter(ContextConversionFactorProvider(context))
+
+inline fun <R> Context.screenDimensionUnitConversion(conversionBlock: ScreenDimensionUnitConverter.() -> R): R =
+    conversionBlock(BaseScreenDimensionUnitConverter(ContextConversionFactorProvider(this)))
+
+inline fun <R> View.screenDimensionUnitConversion(conversionBlock: ScreenDimensionUnitConverter.() -> R): R =
+    conversionBlock(BaseScreenDimensionUnitConverter(ContextConversionFactorProvider(context!!)))
+
+inline fun <U : ScreenDimensionUnit, R : ScreenDimensionUnit> Context.convert(
+    unit: U,
+    conversionBlock: ScreenDimensionUnitConverter.(U) -> R
+): R = conversionBlock(BaseScreenDimensionUnitConverter(ContextConversionFactorProvider(this)), unit)
+
+inline fun <U : ScreenDimensionUnit, R : ScreenDimensionUnit> View.convert(
+    unit: U,
+    conversionBlock: ScreenDimensionUnitConverter.(U) -> R
+): R = conversionBlock(BaseScreenDimensionUnitConverter(ContextConversionFactorProvider(context)), unit)
--- src/androidMain/kotlin/com.chrynan.inlinepixel/PixelConversionUtils.kt
+++ src/androidMain/kotlin/com.chrynan.inlinepixel/PixelConversionUtils.kt
@@ -4,7 +4,6 @@ package com.chrynan.inlinepixel
 
 import android.content.Context
 import android.content.res.Resources
-import android.view.View
 
 fun Context.convertDipToPx(dip: DependencyIndependentPixels) = resources.convertDipToPx(dip)
 
@@ -93,32 +92,3 @@ fun Resources.convertToPt(unit: ScreenDimensionUnit) =
         is ScaledPixels -> convertSpToPt(unit)
         is Pixels -> convertPxToPt(unit)
     }
-
-class ContextConversionFactorProvider(context: Context) : ConversionFactorProvider {
-
-    override val dipToPxFactor = context.resources.displayMetrics.density
-
-    override val spToPxFactor = context.resources.displayMetrics.scaledDensity
-
-    override val ptToPxFactor =
-        context.resources.displayMetrics.densityDpi / ConversionFactorProvider.POINT_PIXELS_PER_INCH
-}
-
-class ContextScreenDimensionUnitConverter(context: Context) :
-    BaseScreenDimensionUnitConverter(ContextConversionFactorProvider(context))
-
-inline fun <R> Context.screenDimensionUnitConversion(conversionBlock: ScreenDimensionUnitConverter.() -> R): R =
-    conversionBlock(BaseScreenDimensionUnitConverter(ContextConversionFactorProvider(this)))
-
-inline fun <R> View.screenDimensionUnitConversion(conversionBlock: ScreenDimensionUnitConverter.() -> R): R =
-    conversionBlock(BaseScreenDimensionUnitConverter(ContextConversionFactorProvider(context!!)))
-
-inline fun <U : ScreenDimensionUnit, R : ScreenDimensionUnit> Context.convert(
-    unit: U,
-    conversionBlock: ScreenDimensionUnitConverter.(U) -> R
-): R = conversionBlock(BaseScreenDimensionUnitConverter(ContextConversionFactorProvider(this)), unit)
-
-inline fun <U : ScreenDimensionUnit, R : ScreenDimensionUnit> View.convert(
-    unit: U,
-    conversionBlock: ScreenDimensionUnitConverter.(U) -> R
-): R = conversionBlock(BaseScreenDimensionUnitConverter(ContextConversionFactorProvider(context)), unit)
